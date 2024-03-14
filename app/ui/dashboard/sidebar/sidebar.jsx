94db442bd55c8207ed1c929d7c0d9581bc04d608
Yuxin <yuxin_ye@protonmail.com>
2024-03-14 20:54:15 +1100

    add logout button

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -102,6 +102,10 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
     </div>
   );
 };
