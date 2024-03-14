7456287a18bc1565845b2a22c015bf00909ad491
Yuxin <yuxin_ye@protonmail.com>
2024-03-14 21:26:57 +1100

    add card

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,7 +1,19 @@
 import styles from "./card.module.css";
+import { MdSupervisedUserCircle } from "react-icons/md";
 
 const Card = () => {
-  return <div className={styles.container}>Card</div>;
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Income</span>
+        <span className={styles.number}>1,200,470</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>12%</span> more than previous
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
