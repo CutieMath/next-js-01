b033c71586d2e3507029665591398aa3eb9efb20
Yuxin <yuxin_ye@protonmail.com>
2024-04-04 20:23:04 +1100

    create users page search bar and table

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,10 +1,61 @@
+import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const UsersPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.top}></div>
-      <table className={styles.table}></table>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user" />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Create At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/profile-1.png"
+                  alt="Profile"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Cutie Mimi
+              </div>
+            </td>
+            <td>cutie@email.com</td>
+            <td>06/06/2023</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
