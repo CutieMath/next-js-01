9f3ccd320d1453632cac0cad23f5a8a8b01cce7d
Yuxin <yuxin_ye@protonmail.com>
2024-04-09 20:32:31 +1000

    add a new page in Nxt.js to addANewProduct

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,7 @@
+import styles from "../../../ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddProductPage = () => {
+  return <div className={styles.container}>Add Product</div>;
+};
+
+export default AddProductPage;
