b1ffc828ee145fcb126d3f6cf8da3cfad8052c24
Yuxin <yuxin_ye@protonmail.com>
2024-04-09 20:38:27 +1000

    construct addNew page

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -1,7 +1,19 @@
 import styles from "../../../ui/dashboard/products/addProduct/addProduct.module.css";
 
 const AddProductPage = () => {
-  return <div className={styles.container}>Add Product</div>;
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a Category</option>
+          <option value="pie">Pie</option>
+          <option value="mimi">Mimi</option>
+          <option value="mino">Mino</option>
+        </select>
+      </form>
+    </div>
+  );
 };
 
 export default AddProductPage;
